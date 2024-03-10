b742e223bfeabb1a7d154497314d96fda3a193f7
angelina <angln.pnn@gmail.com>
2024-03-10 18:29:31 +0700

    Функция отправки вынесена в bot.py

---
+++ habits/bot.py
@@ -0,0 +1,15 @@
+from config.settings import TELEGRAM_BOT_API_TOKEN
+import requests
+
+
+def send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_API_TOKEN}/sendMessage'
+    params = {'chat_id': chat_id, 'text': text}
+
+    try:
+        response = requests.post(url, params=params)
+        response.raise_for_status()  # Проверяем статус ответа
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Request error: {e}")
+        return None
